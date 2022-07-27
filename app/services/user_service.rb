6613a17bd2b722932afcd192270f4ce7ba25e974
Sarah Hall <swatson1994@yahoo.com>
2022-07-27 18:19:52 -0400

    Feat: Add service method for single user return

--- app/services/user_service.rb
+++ app/services/user_service.rb
@@ -4,4 +4,9 @@ class UserService < BaseService
     response = conn_list.get("users")
     data = get_json(response)[:data]
   end
+
+  def self.get_user(id)
+    response = conn_list.get("users/#{id}")
+    data = get_json(response)[:data]
+  end
 end
