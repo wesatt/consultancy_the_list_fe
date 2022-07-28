5b628c47696a1c2c6d0b1c72b1db6c227f4e06ac
Deannah Burke <deannah@bierstadtlager.com>
2022-07-28 17:03:55 -0600

    Add feature test and logo to welcome page

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -6,15 +6,9 @@ RSpec.describe "Welcome Page", type: :feature do
 
     expect(page).to have_content("Welcome to The List!")
     expect(page).to have_content("About Us")
-    expect(page).to have_content("An app to recommend and share your favorite books and movies with friends! Register or log in with Google.")
+    within '#about-us' do
+      expect(page).to have_content("An app to recommend and share your favorite books and movies with friends! Register or log in with Google.")
+    end
     expect(page).to_not have_content("Logout")
-  end
-
-  it 'has a button to sign in with Google' do
-    visit root_path
-
-    expect(page).to have_button("Sign In with Google")
-    click_button("Sign In with Google")
-    expect(current_path).to eq('/login')
   end
 end
