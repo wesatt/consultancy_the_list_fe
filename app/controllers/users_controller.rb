12cffc4caff2960e9f99a6f2503673ca27982cfe
Sarah Hall <swatson1994@yahoo.com>
2022-07-28 13:22:38 -0400

    Feat: Add new facade to controller and remove comment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,9 @@ class UsersController < ApplicationController
   def show
     auth_hash = request.env['omniauth.auth']
     if auth_hash['credentials']['token'].present?
-      @email = auth_hash['info']['email'] #how to use find_or_create_by/create facade method with conditional?
-      @name = auth_hash['info']['name']
+      email = auth_hash['info']['email']
+      name = auth_hash['info']['name']
+      UserFacade.find_or_create_user({name: name, email: email})
       redirect_to '/dashboard'
     end
   end
