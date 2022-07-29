35affff4149c09832d5fbabec011e81f9833778d
Sarah Hall <swatson1994@yahoo.com>
2022-07-28 20:41:51 -0400

    Add open library to base service

--- app/services/base_service.rb
+++ app/services/base_service.rb
@@ -3,6 +3,10 @@ class BaseService
     Faraday.new(url: "https://the-list-be.herokuapp.com/api/v1/")
   end
 
+  def self.conn_book
+    Faraday.new(url: "http://openlibrary.org/")
+  end
+
   def self.get_json(response)
     JSON.parse(response.body, symbolize_names: true)
   end
