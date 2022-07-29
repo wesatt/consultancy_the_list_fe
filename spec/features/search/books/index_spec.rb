c1bf35632abbe2632840c345f59cd05a6bd79558
Sarah Hall <swatson1994@yahoo.com>
2022-07-29 11:25:25 -0400

    Refactor: Remove save and open page

--- spec/features/search/books/index_spec.rb
+++ spec/features/search/books/index_spec.rb
@@ -4,7 +4,6 @@ require 'rails_helper'
 RSpec.describe "Book Search Page", type: :feature do
   it 'has a search box' do
     visit '/search/books'
-    save_and_open_page
 
     expect(page).to have_field('Title')
     expect(page).to have_button('Search Titles')
