148363d37db6bfd8bf0963377c96555dc0b17dbf
Sarah Hall <swatson1994@yahoo.com>
2022-07-28 20:51:33 -0400

    Update test for vcr and create book service

--- spec/services/books_service_spec.rb
+++ spec/services/books_service_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe BookService do
+RSpec.describe BookService, :vcr do
   describe 'search by phrase endpoint' do
     it 'can retrieve information from book search endpoint' do
-      json = BookService.get_search_books("fish")
+      json = BookService.get_search_books("fish and chips")
 
       expect(json).to be_an(Array)
       expect(json[0]).to have_key(:title)
