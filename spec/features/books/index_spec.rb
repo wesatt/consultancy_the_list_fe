fb25ebc8480628d4ae103113f453f6ae487fa148
Sarah Hall <swatson1994@yahoo.com>
2022-07-30 14:30:09 -0400

    Update: Test to include mocked search

--- spec/features/books/index_spec.rb
+++ spec/features/books/index_spec.rb
@@ -2,9 +2,16 @@ require 'rails_helper'
 # save_and_open_page
 
 RSpec.describe "Books search results page", type: :feature do
-  it 'will display a list of books, and authors' do
+  before(:each) do
+    allow_any_instance_of(ApplicationController).to receive(:session_auth).and_return(true)
+  end
+
+  it 'will display a list of books, and authors', :vcr do
     #mock search hit?
+    books = BookFacade.create_book_results("The Book Thief")
+    allow(BookFacade).to receive(:create_book_results).and_return(books)
     visit '/books'
+    save_and_open_page
 
     expect(page).to have_content("The Book Thief")
     expect(page).to have_content("Markus Zusak")
