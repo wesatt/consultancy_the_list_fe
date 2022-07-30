758ab7cfc28ce6e301f15f19c0bf07ebd9cb30db
Sarah Hall <swatson1994@yahoo.com>
2022-07-30 17:29:11 -0400

    Feat: Update service, facade, and poro to include method for show page

--- app/poros/book.rb
+++ app/poros/book.rb
@@ -2,13 +2,21 @@ class Book
   attr_reader :id,
               :title,
               :author,
-              :description
+              :description,
+              :subtitle,
+              :published_date,
+              :page_count,
+              :average_rating
 
   def initialize(data)
     @id = data[:id]
     @title = data[:volumeInfo][:title]
     @author = format_author(data[:volumeInfo][:authors])
     @description = data[:volumeInfo][:description]
+    @subtitle = data[:volumeInfo][:subtitle]
+    @published_date = data[:volumeInfo][:publishedDate]
+    @page_count = data[:volumeInfo][:pageCount]
+    @average_rating = data[:volumeInfo][:averageRating]
   end
 
   def format_author(author)
--- app/services/book_service.rb
+++ app/services/book_service.rb
@@ -4,4 +4,9 @@ class BookService < BaseService
     response = conn_book.get("volumes?q=#{search_query}")
     data = get_json(response)[:items]
   end
+
+  def self.get_book_details(id)
+    response = conn_book.get("volumes/#{id}")
+    data = get_json(response)
+  end
 end
