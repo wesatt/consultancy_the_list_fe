00a5ec78023c6a74612c5d78732e85c1b2a067e5
Sarah Hall <swatson1994@yahoo.com>
2022-07-30 15:21:59 -0400

    Test: Add edge case formatting author tests

--- spec/poros/book_spec.rb
+++ spec/poros/book_spec.rb
@@ -5,23 +5,43 @@ RSpec.describe Book do
     data = {
             key: "/works/OL5819456W",
             title: "Book On Frog",
-            author_name: "Deannah Burke"
+            author_name: ["Deannah Burke"]
           }
     book = Book.new(data)
     expect(book).to be_a(Book)
     expect(book.key).to eq("OL5819456W")
     expect(book.title).to eq("Book On Frog")
-    expect(book.author).to eq("Deannah Burke")
+    expect(book.author).to eq(["Deannah Burke"])
   end
 
   it 'will not include /works/ in the key' do
     data = {
             key: "/works/OL5819456W",
             title: "Book On Frog",
-            author_name: "Deannah Burke"
+            author_name: ["Deannah Burke"]
           }
     book = Book.new(data)
     expect(book.key).to eq("OL5819456W")
     expect(book.key).to_not eq("/works/OL5819456W")
+  end
+
+  it 'will replace an absent author with N/A' do
+    data = {
+            key: "/works/OL5819456W",
+            title: "Book On Frog",
+            author_name: nil
+          }
+    book = Book.new(data)
+    expect(book.author).to eq(["N/A"])
+  end
+
+  it 'will replace empty author array with N/A' do
+    data = {
+            key: "/works/OL5819456W",
+            title: "Book On Frog",
+            author_name: []
+          }
+    book = Book.new(data)
+    expect(book.author).to eq(["N/A"])
   end
 end
