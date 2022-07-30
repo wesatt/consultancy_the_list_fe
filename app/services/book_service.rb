39b830dad21789a1e323194245baab1b47e80d63
Sarah Hall <swatson1994@yahoo.com>
2022-07-30 16:57:11 -0400

    Fix: Update tests and views around api change

--- app/services/book_service.rb
+++ app/services/book_service.rb
@@ -1,7 +1,7 @@
 class BookService < BaseService
 
   def self.get_search_books(search_query)
-    response = conn_book.get("volumes?q=#{search_query}+intitle")
+    response = conn_book.get("volumes?q=#{search_query}")
     data = get_json(response)[:items]
   end
 end
