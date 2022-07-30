5d59e70238170de78c50b73532671bfc64d8d7d1
Sarah Hall <swatson1994@yahoo.com>
2022-07-30 15:22:28 -0400

    Feat: Add methods in book poro to handle edge case and formatitng

--- app/poros/book.rb
+++ app/poros/book.rb
@@ -4,8 +4,20 @@ class Book
               :author
 
   def initialize(data)
-    @key = data[:key]
+    @key = format_key(data[:key])
     @title = data[:title]
-    @author = data[:author_name]
+    @author = format_author(data[:author_name])
+  end
+
+  def format_key(key)
+    key.sub('/works/', '')
+  end
+
+  def format_author(author)
+    if author.nil? || author.empty?
+      ["N/A"]
+    else
+      author
+    end
   end
 end
