3cdd927d42732b325909528c6d710228417c04c5
Sarah Hall <swatson1994@yahoo.com>
2022-07-30 17:28:43 -0400

    Test: Update to include more detail for book show page

--- spec/poros/book_spec.rb
+++ spec/poros/book_spec.rb
@@ -9,7 +9,11 @@ RSpec.describe Book do
                 authors: [
                     "Deannah"
                 ],
-                description: "Not Much to say about it."}
+                description: "Not Much to say about it.",
+                subtitle: "It's about frogs",
+                publishedDate: "2022-03-15",
+                pageCount: "75",
+                averageRating: "5"}
         }
     book = Book.new(data)
     expect(book).to be_a(Book)
@@ -17,6 +21,10 @@ RSpec.describe Book do
     expect(book.title).to eq("Frogs, meet Clogs")
     expect(book.author).to eq(["Deannah"])
     expect(book.description).to eq("Not Much to say about it.")
+    expect(book.subtitle).to eq("It's about frogs")
+    expect(book.published_date).to eq("2022-03-15")
+    expect(book.page_count).to eq("75")
+    expect(book.average_rating).to eq("5")
   end
 
   it 'will replace an absent author with N/A' do
