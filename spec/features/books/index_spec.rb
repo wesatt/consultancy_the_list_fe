06e13f2d0ff12a97c86583c5a7a26d3f6a202063
Sarah Hall <swatson1994@yahoo.com>
2022-07-30 14:56:47 -0400

    Test: Add test for working title link from results page

--- spec/features/books/index_spec.rb
+++ spec/features/books/index_spec.rb
@@ -7,13 +7,24 @@ RSpec.describe "Books search results page", type: :feature do
   end
 
   it 'will display a list of books, and authors', :vcr do
-    #mock search hit?
     books = BookFacade.create_book_results("The Book Thief")
     allow(BookFacade).to receive(:create_book_results).and_return(books)
     visit '/books'
-    save_and_open_page
 
     expect(page).to have_content("The Book Thief")
     expect(page).to have_content("Markus Zusak")
+  end
+
+  it 'Has the books title as a link to its show page', :vcr do
+    books = BookFacade.create_book_results("The Book Thief")
+    allow(BookFacade).to receive(:create_book_results).and_return(books)
+    visit '/books'
+
+    within page.all('.bookResults')[0] do
+      expect(page).to have_link("The Book Thief")
+      click_link("The Book Thief")
+
+      expect(page).to have_current_path('/books/works/OL5819456W') #identifier from api as id
+    end
   end
 end
