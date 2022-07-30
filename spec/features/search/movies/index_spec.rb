7a805db2cf08392f46a34663811308503ad50300
Deannah Burke <deannah@bierstadtlager.com>
2022-07-29 20:54:10 -0600

    Add note for movie index testing session auth hack
    Co-authored-by: Sai Hall <99222755+SaiHall@users.noreply.github.com>

--- spec/features/search/movies/index_spec.rb
+++ spec/features/search/movies/index_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'Movies Search Index Page', type: :feature do
+  #before(:each) do
+    #allow_any_instance_of(ApplicationController).to receive(:session_auth).and_return(true)
+  #end
+  ##this is how we spoof passing the session auth! 
   xit 'has a form to search movies by title' do
     visit '/search/movies'
 
