1b444f54581317b18cc17af60291732c03ceee04
Sarah Hall <swatson1994@yahoo.com>
2022-07-30 17:59:21 -0400

    Test: Update test to include more info

--- spec/features/books/show_spec.rb
+++ spec/features/books/show_spec.rb
@@ -6,12 +6,15 @@ RSpec.describe "Books show page", type: :feature do
     allow_any_instance_of(ApplicationController).to receive(:session_auth).and_return(true)
   end
 
-  it 'displays more details about a single book' do
+  it 'displays more details about a single book', :vcr do
     visit '/books/LnVPj0lN0eIC'
+    save_and_open_page
 
     expect(page).to have_content("Krampus")
     expect(page).to have_content("The Yule Lord")
     expect(page).to have_content("Brom")
     expect(page).to have_content("Terrific. A wild ride….I loved it.")
+    expect(page).to have_content("3.5/5")
+    expect(page).to have_content("Number of Pages: 368")
   end
 end
