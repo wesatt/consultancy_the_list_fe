f1dec267787dd20d896b417e141962da8fb7da88
Sarah Hall <swatson1994@yahoo.com>
2022-08-03 19:59:14 -0400

    Refactor: remove commented out code, put rec in right place, update hash

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -4,7 +4,7 @@ class RecommendationsController < ApplicationController
     if params[:recommended].blank?
       redirect_to "/#{params[:media_type]}s/#{params[:media_id]}", notice: 'You must select someone to make a recommendation.'
     else
-      UserFacade.create_recommendations(params, current_user)
+      RecommendationFacade.create_recommendations(params, current_user)
       redirect_to '/dashboard'
     end
   end
