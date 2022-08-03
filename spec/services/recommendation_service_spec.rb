2c7f9c6b424d0a8ec6895698e54efbf77ad54127
Sarah Hall <swatson1994@yahoo.com>
2022-08-03 19:24:05 -0400

    Test: Update tests to include live api hits

--- spec/services/recommendation_service_spec.rb
+++ spec/services/recommendation_service_spec.rb
@@ -4,29 +4,6 @@ require 'rails_helper'
 
 RSpec.describe RecommendationService, :vcr do
   it ".get_user_recs(id) returns a json of all of a user's recommendations" do
-    user_recs_json = [
-      { id: '1',
-        type: 'recommendation',
-        attributes: { media_id: '11868', title: 'Dracula', media_type: 'movie', recommended_by_id: 13,
-                      status: 'pending', user_id: 1 } },
-      { id: '3',
-        type: 'recommendation',
-        attributes: { media_id: '654810', title: 'Lasagna', media_type: 'movie', recommended_by_id: 5,
-                      status: 'pending', user_id: 1 } },
-      { id: '4',
-        type: 'recommendation',
-        attributes: { media_id: '545611', title: 'Everything Everywhere All at Once', media_type: 'movie',
-                      recommended_by_id: 6, status: 'pending', user_id: 1 } },
-      { id: '8',
-        type: 'recommendation',
-        attributes: { media_id: '354333', title: 'Spike', media_type: 'movie', recommended_by_id: 8,
-                      status: 'pending', user_id: 1 } },
-      { id: '10',
-        type: 'recommendation',
-        attributes: { media_id: '8067', title: 'A Life Less Ordinary', media_type: 'movie', recommended_by_id: 8,
-                      status: 'pending', user_id: 1 } }
-    ]
-    allow(RecommendationService).to receive(:get_user_recs).and_return(user_recs_json)
     rec_json = RecommendationService.get_user_recs(1)
 
     expect(rec_json).to be_a(Array)
@@ -46,16 +23,6 @@ RSpec.describe RecommendationService, :vcr do
       recommended_by_id: 1
     }
     rec_to_id = '2'
-    return_value = { data: { id: '16',
-                             type: 'recommendation',
-                             attributes: { media_id: '11868',
-                                           title: 'Dracula',
-                                           media_type: 'movie',
-                                           recommended_by_id: 1,
-                                           status: 'pending',
-                                           user_id: 2 } } }
-
-    allow(RecommendationService).to receive(:post_recommendation).and_return(return_value)
     created_rec = RecommendationService.post_recommendation(media_data, rec_to_id)
 
     expect(created_rec).to be_a(Hash)
