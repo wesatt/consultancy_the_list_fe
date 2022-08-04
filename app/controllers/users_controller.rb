29f9de3e49dde31c28eb59632a3e0b74c0b6d132
Wesley Garcia <wes@alltradestech.com>
2022-08-04 14:47:58 -0700

    Refactor sessions to be created in SessionsController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,6 @@ class UsersController < ApplicationController
   before_action :current_user, :session_auth, except: :index
 
   def index
-    auth_hash = request.env['omniauth.auth']
-    if auth_hash['credentials']['token'].present?
-      email = auth_hash['info']['email']
-      name = auth_hash['info']['name']
-      user = UserFacade.find_or_create_user({name: name, email: email})
-      session[:user] = user
-      redirect_to '/dashboard'
-    end
   end
 
   def show
