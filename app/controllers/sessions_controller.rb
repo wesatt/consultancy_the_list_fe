d61f90920c8d888adaeed65952a94de3a426dfd1
Sarah Hall <swatson1994@yahoo.com>
2022-08-04 13:43:49 -0400

    Refactor: Remove commented out code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,5 @@
 class SessionsController < ApplicationController
   def destroy
-    # reset_session
-    # session.clear
-    # session.delete(:user)
-    # @current_user = nil
     session.destroy
     redirect_to '/'
   end
