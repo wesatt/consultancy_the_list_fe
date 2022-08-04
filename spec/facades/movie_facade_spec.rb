c3267f0063b06f1e282cee6a4f376ab6d5dd59ed
Sarah Hall <swatson1994@yahoo.com>
2022-08-04 15:02:18 -0400

    Comment out tests for stretch goal endpoints

--- spec/facades/movie_facade_spec.rb
+++ spec/facades/movie_facade_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe MovieFacade, :vcr do
   describe 'Facade methods' do
-    it '.top_rated returns an array of movie poros of the top 20 movies' do
-      movies = MovieFacade.top_rated
-
-      expect(movies).to be_a(Array)
-      expect(movies).to be_all(Movie)
-    end
+    # it '.top_rated returns an array of movie poros of the top 20 movies' do
+    #   movies = MovieFacade.top_rated
+    #
+    #   expect(movies).to be_a(Array)
+    #   expect(movies).to be_all(Movie)
+    # end
 
     it '.movie_lookup(id) returns a single movie poros for the given id' do
       movie = MovieFacade.movie_lookup(11_868)
