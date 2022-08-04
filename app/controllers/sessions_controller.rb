d21743120ae6c6a09f5fb483f064fbb3e12acc66
Sarah Hall <swatson1994@yahoo.com>
2022-08-04 13:14:23 -0400

    Testing: Removal of current-user from welcome index

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,8 @@
 class SessionsController < ApplicationController
   def destroy
     # reset_session Nope
-    session.clear
-    # session.destroy
+    # session.clear
+    session.destroy
     redirect_to '/'
   end
 end
